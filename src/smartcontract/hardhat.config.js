f6992e2e1b99755c73521fd89a2a485af61f3ed2
Vanshika <vanshikasrivastava.business@gmail.com>
2024-04-03 20:58:12 +0530

    added dotenv

--- src/smartcontract/hardhat.config.js
+++ src/smartcontract/hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
