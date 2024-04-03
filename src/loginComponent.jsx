283a1dd99da223990b2185747a853285bb251769
Vanshika <vanshikasrivastava.business@gmail.com>
2024-04-04 01:00:53 +0530

    primary wallet address fixed

--- src/loginComponent.jsx
+++ src/loginComponent.jsx
@@ -1,11 +1,10 @@
 import React, { useState,useEffect } from "react";
 import {
-  DynamicContextProvider,
+DynamicContextProvider,
   DynamicWidget,
   DynamicUserProfile,
   useDynamicContext,
 } from "@dynamic-labs/sdk-react-core";
-
 
 
 const LoginComponent = () => {
@@ -14,28 +13,14 @@ const LoginComponent = () => {
 
   return (
     <div>
+    <div>
           <DynamicWidget />
           <DynamicUserProfile />
-          <ProfileDynamic />
+    </div>
+          {/* <p>{primaryWallet?.address}</p> */}
         
     </div>
   );
 };
-
-function ProfileDynamic() {
-  const { primaryWallet } = useDynamicContext();
- 
-  useEffect(() => {
-    if (primaryWallet && primaryWallet.address) {
-      address = primaryWallet.address; // Updating the address variable
-    }
-  }, [primaryWallet]);
-
-  // Logic for handling profile details when Dynamic Auth is chosen
-  // You can use showDynamicUserProfile to trigger actions when the widget opens or closes
-
-  return null; // Since there's no JSX to return in this example
-}
-
 
 export default LoginComponent;
