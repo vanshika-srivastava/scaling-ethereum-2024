695d3d459835cd0c02df202a68142aecebc78ea1
Vanshika <vanshikasrivastava.business@gmail.com>
2024-04-03 20:56:06 +0530

    hardhat config

--- src/smartcontract/hardhat.config.js
+++ src/smartcontract/hardhat.config.js
@@ -3,4 +3,18 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  
+  networks: { 
+      mainnet: {
+      url: "https://rpc.ankr.com/gnosis",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      }
+     },
+
+  gnosisscan: {
+    apiKey: {
+      chiado: process.env.GNOSISSCAN_API_KEY || "",
+      gnosis: process.env.GNOSISSCAN_API_KEY || "",
+    },
+  },
 };
