917f232602f037740131d434cf42ae0dfd87bccd
Vanshika <vanshikasrivastava.business@gmail.com>
2024-04-04 17:16:30 +0530

    minor configurations, fixed the nft mint

--- src/mintComponent.jsx
+++ src/mintComponent.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { ethers } from "ethers";
 import abi from './smartcontract/utils/abi.json'
@@ -12,7 +14,7 @@ function Mint() {
     const initializeContract = async () => {
       // Initialize your NFT contract here
     //   const nftAddress = import.meta.env.VITE_NFT_CONTRACT_ADDRESS;
-      const nftAddress = "0x34348d42902a668C72372a91773E4133f7191f98" ;
+      const nftAddress = "0x3f6E49B7A4d8e5722cFd10DD4e1E4E362238dE7e" ;
       const signer = await primaryWallet.connector.ethers?.getSigner();
       const provider = await primaryWallet.connector.ethers?.getWeb3Provider();
       const contract = new ethers.Contract(nftAddress, abi, signer);
@@ -28,14 +30,46 @@ function Mint() {
       return;
     }
   
-    console.log("Contract is initialized:", contract);
-  
     try {
       const txRequest = await contract.mint();
       const signer = await primaryWallet.connector.ethers?.getSigner();
-      const tx = await signer.sendTransaction(txRequest);
-      setTxHash(tx.hash);
-      console.log(`Transaction sent: ${tx.hash}`);
+      // const tx = await signer.sendTransaction(txRequest);
+      // console.log("txxxx",tx);
+  
+      // console.log("Transaction object:", tx);
+  
+      // // Check if the transaction object is valid
+      // if (!tx || !tx.hash) {
+      //   console.error("Invalid transaction object:", tx);
+      //   return;
+      // }
+  
+      console.log(`Transaction sent: ${txRequest.hash}`);
+      toast.info("Transaction is being processed...");
+  
+      // Listen for transaction confirmation
+      const provider = await primaryWallet.connector.ethers?.getWeb3Provider();
+      const txReceipt = await provider.waitForTransaction(txRequest.hash);
+  
+      console.log("Transaction receipt:", txReceipt);
+  
+      // Set the transaction hash once confirmed
+      setTxHash(txRequest.hash);
+      toast.success(
+        <div>
+          <p>Congrats, you minted your dynamic nft! 🎉</p>
+            <p>Transaction Hash:</p>
+          <p>
+            <a
+              href={`https://gnosis.blockscout.com/tx/${txRequest.hash}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {txRequest.hash}
+            </a>
+          </p>
+        </div>
+      );
     } catch (error) {
       console.error(`Error sending transaction: ${error}`);
     }
@@ -46,23 +80,19 @@ function Mint() {
     <div className="flex justify-center items-center ">
       <button
         onClick={handleMint}
-        className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-4 rounded"
+        className="bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-3 rounded mt-4"
       >
-        Mint NFT
+        Mint your Dynamic NFT
       </button>
-      {txHash && (
-        <div className="mt-4">
-          Transaction Hash:{" "}
-          <a
-            href={`https://etherscan.io/tx/${txHash}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500"
-          >
-            {txHash}
-          </a>
-        </div>
-      )}
+      <div className="absolute top-0 right-0 mt-4 mr-4">
+        <button
+        onClick={() => window.open("https://faucet.gnosischain.com/", "_blank")}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+    Gnosis Chain Faucet
+    </button>
+    </div>
+
+     <ToastContainer/>
     </div>
   );
 }
--- src/smartcontract/contracts/DynamicNFT.sol
+++ src/smartcontract/contracts/DynamicNFT.sol
@@ -10,6 +10,7 @@ import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 
+
 contract DynamicNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     // using Counters for Counters.Counter;
     using Strings for uint256;
@@ -21,6 +22,7 @@ contract DynamicNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     function generateSVG(uint256 tokenId) public pure returns (string memory) {
     bytes memory svg = abi.encodePacked(
         '<svg width="1080" height="1080" viewBox="0 0 1080 1080" fill="none" xmlns="http://www.w3.org/2000/svg">',
+        '<style>.base { fill: black; font-family: serif; font-size: 60px; }</style>',
         '<rect width="1080" height="1080" fill="#3E6957"/>',
         '<rect x="46" y="46" width="987" height="987" rx="50" fill="#F0EBDE"/>',
         '<mask id="mask0_1096_34" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="46" y="46" width="987" height="987">',
@@ -32,7 +34,7 @@ contract DynamicNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         '<path d="M915.115 361.294C945.432 396.895 962.092 442.123 962.114 488.883C962.076 541.471 941.159 591.891 903.96 629.063C866.762 666.234 816.326 687.115 763.738 687.115C717.222 687.167 672.184 670.775 636.582 640.837L541.143 736.276L445.703 640.837C410.077 670.829 364.973 687.226 318.403 687.115C292.303 687.209 266.442 682.153 242.3 672.235C218.158 662.317 196.211 647.733 177.715 629.318C159.22 610.903 144.54 589.019 134.517 564.921C124.494 540.822 119.325 514.983 119.306 488.883C119.344 442.339 135.722 397.285 165.585 361.583L121.036 317.035L78.5073 273.784C27.275 358.017 0.238835 454.737 0.367523 553.326C0.34857 624.189 14.2949 694.361 41.4085 759.831C68.5221 825.302 108.272 884.787 158.386 934.888C208.5 984.989 267.996 1024.72 333.474 1051.82C398.952 1078.91 469.126 1092.84 539.989 1092.8C682.994 1092.77 820.14 1035.98 921.314 934.914C1022.49 833.848 1079.42 696.764 1079.61 553.759C1080.5 455.185 1053.73 358.338 1002.34 274.217L915.115 361.294Z" fill="#3E6957" fill-opacity="0.1"/>',
         '<path d="M930.832 181.236C880.511 128.328 819.946 86.216 752.828 57.4651C685.71 28.7143 613.441 13.9258 540.424 14.0003C467.392 13.9649 395.114 28.7706 327.978 57.5185C260.842 86.2664 200.247 128.357 149.872 181.236C136.753 195.652 123.923 210.069 111.957 225.495L539.991 653.386L968.027 225.063C956.726 209.557 944.294 194.908 930.832 181.236ZM540.424 553.478L208.838 221.891C252.191 178.076 303.845 143.345 360.78 119.729C417.715 96.1136 478.787 84.0876 540.424 84.3545C602.073 84.0149 663.166 96.0077 720.111 119.627C777.056 143.247 828.702 178.017 872.011 221.891L540.424 553.478Z" fill="#3E6957" fill-opacity="0.1"/>',
         '</g>',
-        '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">Dynamic NFT Minted on Gnosis Chain</text>',
+        '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">NFT Minted on Gnosis Chain</text>',
         '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">with TokenID # ', tokenId.toString(), '</text>',
         '</svg>'
     );
@@ -52,7 +54,7 @@ contract DynamicNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     function getTokenURI(uint256 tokenId) public pure returns (string memory){
         bytes memory dataURI = abi.encodePacked(
             '{',
-                '"name": "DG-NFT #', tokenId.toString(), '",',
+                '"name": "B-NFT #', tokenId.toString(), '",',
                 '"description": "NFT minted on Gnosis Chain",',
                 '"image": "', generateSVG(tokenId), '"',
             '}'
@@ -96,7 +98,7 @@ contract DynamicNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
 
 
-// The following functions are overrides required by Solidity.
+    // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
